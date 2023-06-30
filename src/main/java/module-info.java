e7cec1c7a619134e21ba71fcaad7438fb8ada354
seinecle <1244100+seinecle@users.noreply.github.com>
2023-06-30 11:48:57 +0200

    added a module-info declaration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,8 @@
-module LemmatizerLightweight {
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module net.clementlevallois.lemmatizerlightweight {
     exports net.clementlevallois.lemmatizerlightweight;
 }
